b634ef4ea53db0e0bb4a4743b809cc2e482d4fd4
LarisaZaripova <larik7lolik@gmail.com>
2022-02-13 17:04:48 +0300

    added example 20

---
+++ Example_20/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 20: Задать номер четверти, показать диапазоны для возможных координат
+ Console.WriteLine("Введите номер четверти: ");
+ int num = int.Parse(Console.ReadLine());
+ if ( num == 1)
+ {
+     Console.WriteLine("Координата х > 0 и координата у > 0");
+ }
+ else if (num == 2)
+ {
+     Console.WriteLine("Координата х < 0 и координата у < 0");
+ }
+ else if (num == 3)
+ {
+     Console.WriteLine("Координата х < 0 и координата у > 0");
+ }
+ else if (num == 4)
+ {
+     Console.WriteLine("Координата х < 0 и координата у > 0");
+ }
+ else
+ {
+     Console.WriteLine("Координата х = 0 и координата у = 0");
+ }
+ 
