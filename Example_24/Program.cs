3d438ad68e10df139f965c6f036d14572f4597d5
LarisaZaripova <larik7lolik@gmail.com>
2022-02-13 17:40:11 +0300

    added Example 24

---
+++ Example_24/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 24: Найти кубы чисел от 1 до N
+int num = 10;
+int y = 1;
+int x = 1;
+while (x <= num)
+{
+  Console.Write(x + "\t");
+  x++;
+}
+Console.WriteLine();
+while (y <= num)
+{
+  Console.Write(y*y*y + "\t");
+  y++;
+}
+
