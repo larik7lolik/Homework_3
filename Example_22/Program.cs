87314ef7ab8e4165bf218365d6d2262922125e2f
LarisaZaripova <larik7lolik@gmail.com>
2022-02-14 13:05:44 +0300

    added new Example 22

--- Example_22/Program.cs
+++ Example_22/Program.cs
@@ -1,19 +1,29 @@
 ﻿// Задача 22: Найти расстояние между точками в пространстве 2D/3D
-Console.WriteLine("Выберите пространство 1:  ");
-int num 1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Выберите пространство 2:  ");
-int num 2 = int.Parse(Console.ReadLine());
-int x =  int.Parse(Console.ReadLine());  
-int y =  int.Parse(Console.ReadLine());  
-
-if (num 1 == 2D)
+ Console.WriteLine("Выберите пространство 2D или 3D:  ");
+int num  = int.Parse(Console.ReadLine());
+int[] coordinatesInput()
 {
-   Console.WriteLine(" Вы выбрали пространство 2D. Задайте координаты двух точек: ");
-   if(x >= 0 && y >= 0)
-   {
-      Console.WriteLine("  ");
-   }
+   int[] xyz = new int[3];
+   Console.Write("Введите координату Х: ");
+   xyz[0]=int.Parse(Console.ReadLine());
+   Console.Write("Введите координату Y: ");
+   xyz[1]=int.Parse(Console.ReadLine());
+   Console.Write("Введите координату Z: ");
+   xyz[2]=int.Parse(Console.ReadLine());
+   return xyz;
 }
+double range(int[] n1, int[] n2)
+{
+    return Math.Sqrt((n1[0]-n2[0])*(n1[0]-n2[0]) + (n1[1]-n2[1])*(n1[1]-n2[1]) + (n1[2]-n2[2])*(n1[2]-n2[2]));
+}
+Console.WriteLine("Первая точка: ");
+int[] firstPoint = coordinatesInput();
+Console.WriteLine();
+Console.WriteLine("Вторая точка: ");
+int[] secondPoint = coordinatesInput();
+Console.WriteLine();
+Console.WriteLine($"Расстояние между указанными точками составляет: {range(firstPoint, secondPoint):f2}");
+Console.WriteLine();
 
 
 
